7b21d3470dded4f38c6a5927d8744321315ec4ea
Adrian Bolinger <adrian.bolinger@me.com>
2016-12-12 14:59:13 -0500

    Added demo images, configured basic buttons

--- OCRDemo/MainViewController.swift
+++ OCRDemo/MainViewController.swift
@@ -16,12 +16,26 @@ class MainViewController: UIViewController {
     @IBOutlet weak var buttonThree: UIButton!
     @IBOutlet weak var buttonFour: UIButton!
     
-
     // MARK: - Lifecycle Methods
     override func viewDidLoad() {
         super.viewDidLoad()
+        configureButtons()
 
         // Do any additional setup after loading the view.
+    }
+    
+    // MARK: - Configuration methods
+    func configureButtons() {
+        let buttonArray = [buttonOne, buttonTwo, buttonThree, buttonFour]
+        
+        for button in buttonArray {
+            if let buttonTag = button?.tag {
+                let filename = String(format: "image%d.jpg", buttonTag)
+                let buttonImage = UIImage(named: filename)?.withRenderingMode(.alwaysOriginal)
+                button?.setImage(buttonImage, for: .normal)
+                button?.imageView?.contentMode = .scaleAspectFit
+            }
+        }
     }
     
     // MARK: - Button Actions
